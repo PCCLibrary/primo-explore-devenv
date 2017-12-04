11fd3ac5f50cbf6f2013ee59b524da9302ac3c09
gustavo.lanzas <gustavo.lanzas@gmail.com>
2017-12-04 12:34:33 -0800

    first run, updated require-dir to 3.3.2, working on primo sandbox

--- gulp/config.js
+++ gulp/config.js
@@ -129,7 +129,7 @@ function customNpmCssPath() {
 
 
 
-var SERVERS = {
+const SERVERS = {
     local: 'http://localhost:8002'
 };
 
@@ -141,7 +141,7 @@ var SERVERS = {
  *   var PROXY_SERVER = 'http://abc-primo.hosted.exlibrisgroup.com'
  *   var PROXY_SERVER = 'https://abc-primo.hosted.exlibrisgroup.com:443'
  */
-var PROXY_SERVER = 'http://your-server:your-port';
+const PROXY_SERVER = 'https://alliance-primo-sb.hosted.exlibrisgroup.com:443';
 
 
 let buildParams = {
