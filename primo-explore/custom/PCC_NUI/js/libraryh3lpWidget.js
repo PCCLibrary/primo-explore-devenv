adb6b4f80128822374f48ea57e5fd38cd0914f3f
gustavo.lanzas <gustavo.lanzas@gmail.com>
2018-06-15 12:16:59 -0700

    Chat update, dependent on wwwtest.pcc.edu/library/chat-api/widget.php

--- primo-explore/custom/PCC_NUI/js/libraryh3lpWidget.js
+++ primo-explore/custom/PCC_NUI/js/libraryh3lpWidget.js
@@ -1,8 +1,7 @@
 export let libraryh3lpWidgetConfig = {
   name: 'libraryh3lpWidgetConfig',
   config: {
-    url: 'https://libraryh3lp.com/chat/pcc-ask-a-librarian@chat.libraryh3lp.com?skin=28248&title=Chat+With+A+Librarian&identity=PCC',
-    fallback: 'https://wwwtest.pcc.edu/library/scripts/chat-api/questionpoint.html',
+    url: 'https://wwwtest.pcc.edu/library/scripts/chat-api/widget.php',
     prompt: 'Chat',
     icon: {
       set: 'communication',
